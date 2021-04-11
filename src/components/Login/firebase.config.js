e1002c293afd51ebd0edb9d8d6b47cf672c70685
Shahinoor Alam <73857004+shahinooriatc@users.noreply.github.com>
2021-04-12 02:06:25 +0600

    Update firebase.config.js

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,9 +1,1 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCreROUPzELOFzVMFoeSVHHk1EvbyybnwI",
-  authDomain: "burj-al-arab-after-auth-mongo.firebaseapp.com",
-  projectId: "burj-al-arab-after-auth-mongo",
-  storageBucket: "burj-al-arab-after-auth-mongo.appspot.com",
-  messagingSenderId: "749747033443",
-  appId: "1:749747033443:web:9be7c6c65a1b91405e6f56"
-};
-  export default firebaseConfig;
+
